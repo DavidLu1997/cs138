9a6968ddbeb02f3a8032e6708b8596cf3eeac12c
Zopper9000 <david.lu@hotmail.com>
2016-02-27 15:46:01 -0500

    finished q1, on q2

--- a4/a4q1/a4q1.cc
+++ a4/a4q1/a4q1.cc
@@ -11,15 +11,17 @@ struct Qnode {
 struct Queue {
 	Qnode *first;
 	Qnode *last;
+	int size;
 };
 
 void initQ(Queue &q) {
 	q.first = NULL;
 	q.last = NULL;
+	q.size = 0;
 }
 
 bool isEmptyQ(const Queue &q) {
-	return q.first == q.last && q.first == NULL;
+	return q.size == 0;
 }
 
 void enterQ(Queue &q, string val) {
@@ -39,6 +41,8 @@ void enterQ(Queue &q, string val) {
 		q.last->next = n;
 		q.last = n;
 	}
+
+	q.size++;
 }
 
 string firstQ(const Queue &q) {
@@ -63,6 +67,8 @@ void leaveQ(Queue &q) {
 		q.first = q.first->next;
 		delete t;
 	}
+
+	q.size--;
 }
 
 void printQ(const Queue &q) {
@@ -78,6 +84,8 @@ struct PQnode {
 	int priority;
 	Queue q;
 	PQnode *next;
+	int size;
+	int numPriority;
 };
 
 typedef PQnode* PQ;
@@ -98,58 +106,108 @@ void enterPQ(PQ& pq, string val, int priority) {
 		pq->next = NULL;
 		initQ(pq->q);
 		enterQ(pq->q, val);
+		pq->numPriority = 1;
+		pq->size = 1;
 	}
 
-	//Priority exists
-	PQ temp = pq;
-	while (temp->next != NULL) {
-		if (temp->priority == priority) {
-			enterQ(temp->q, val);
-			return;
+	//Not empty
+	else {
+		PQ temp = pq;
+
+		//Check for existence
+		while (temp->next != NULL && priority >= temp->next->priority) { //This seems sketchy
+			temp = temp->next;
 		}
-		temp = temp->next;
+
+		//If priority exists
+		if (temp->priority == priority) {
+			enterQ(temp->next->q, val);
+		}
+
+		//Does not exist, between current and next
+		else if(priority > temp->priority) {
+			PQ t = new PQnode;
+			t->priority = priority;
+			t->next = temp->next;
+			temp->next = t;
+			initQ(t->q);
+			enterQ(t->q, val);
+			pq->numPriority++;
+		}
+
+		//Does not exist, less than current
+		//Only occurs if its the first
+		else {
+			PQ t = new PQnode;
+			t->priority = priority;
+			t->next = temp;
+			pq = t;
+			initQ(t->q);
+			enterQ(t->q, val);
+			pq->numPriority++;
+		}
 	}
 
-	//Priority does not exist, first one
-	if (priority < pq->priority) {
-		PQ t = new PQnode;
-		t->priority = priority;
-		t->next = pq;
-		initQ(t->q);
-		enterQ(t->q, val);
-		pq = t;
-		return;
-	}
-
-	//Priority does not exist, not first one
-	temp = pq;
-	while (temp->next != NULL && temp->next->priority < priority) {
-		temp = temp->next;
-	}
-	PQ t = new PQnode;
-	t->priority = priority;
-	t->next = temp->next;
-	temp->next = t;
-	initQ(t->q);
-	enterQ(t->q, val);
+	pq->size++;
 }
 
 string firstPQ(const PQ& pq) {
-
+	assert(!isEmptyPQ(pq));
+	return pq->q.first->val;
 }
 
 void leavePQ(PQ& pq) {
+	assert(!isEmptyPQ(pq));
 
+	//Delete first element of q
+	leaveQ(pq->q);
+
+	//Check for empty
+	if (isEmptyQ(pq->q)) {
+		PQ temp = pq;
+		pq = pq->next;
+		delete temp;
+		if (pq == NULL) {
+			return;
+		}
+		pq->numPriority--;
+	}
+
+	pq->size--;
 }
 
 int sizePQ(const PQ& pq) {
-
+	return pq->size;
 }
 
 int sizeByPriority(const PQ& pq, int priority) {
 
+	PQ temp = pq;
+
+	while (temp != NULL && temp->priority != priority) {
+		temp = temp->next;
+	}
+
+	return temp == NULL ? 0 : temp->q.size;
 }
 
 int numPriorities(const PQ& pq) {
+	return pq->numPriority;
+}
 
+int main() {
+
+	PQ pq1 = new PQnode;
+	initPQ(pq1);
+	enterPQ(pq1, "derp", 0);
+	enterPQ(pq1, "herp", 2);
+	enterPQ(pq1, "cerp", 1);
+	enterPQ(pq1, "cherp", 1);
+	leavePQ(pq1);
+	enterPQ(pq1, "nigga", 0);
+	leavePQ(pq1);
+	leavePQ(pq1);
+	leavePQ(pq1);
+	leavePQ(pq1);
+	cout << isEmptyPQ(pq1) << endl;
 }
---
+++ a4/a4q2/a4q2.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct BST_Node {
+	string key;
+	BST_Node *left;
+	BST_Node *right;
+};
+
+typedef struct BST_Node* BST;
+
+void BST_init(BST &root) {
+	root = NULL;
+}
+
+bool BST_isEmpty(const BST& root) {
+	return root == NULL;
+}
+
+bool BST_has(const BST& root, string key) {
+	if (BST_isEmpty(root)) {
+		return false;
+	}
+	else if (root->key == key) {
+		return true;
+	}
+	else if (key < root->key) {
+		return BST_has(root->left, key);
+	}
+	else {
+		return BST_has(root->right, key);
+	}
+}
+
+void BST_print(const BST& root) {
+	if (!BST_isEmpty(root)) {
+		BST_print(root->left);
+		cout << "    \"" << root->key << "\"" << endl;
+		BST_print(root->right);
+	}
+}
+
+void BST_insert(BST &root, string key) {
+	if (BST_isEmpty(root)) {
+		root = new BST_Node;
+		root->key = key;
+		root->left = NULL;
+		root->right = NULL;
+	}
+	else if (key < root->key) {
+		BST_insert(root->left, key);
+	}
+	else {
+		BST_insert(root->right, key);
+	}
+}
+
+void BST_delete(BST &target, BST& parent, string key) {
+	//TODO
+}
