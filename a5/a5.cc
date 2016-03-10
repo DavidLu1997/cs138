170a4c916c2d700edf58217bdd7e64a8e2043ae2
David Lu <david.lu@hotmail.com>
2016-03-10 10:38:07 -0500

    made less verbose

--- a5/a5.cc
+++ a5/a5.cc
@@ -2,64 +2,38 @@
 #include <vector>
 #include <string>
 using namespace std;
-
 class Animal {
 public:
-	virtual ~Animal() {
-
-	}
+	virtual ~Animal() { }
 	virtual void speak() const = 0;
 protected:
-	Animal(string name) {
-		this->name = name;
-	}
-	string getName() const {
-		return name;
-	}
+	Animal(string name) { this->name = name; }
+	string getName() const { return name; }
 private:
 	string name;
 };
-
 class Sheep : public Animal {
 public:
-	~Sheep() {
-
-	}
-	void speak() const {
-		cout << "    Sheep " << getName() << " says \"baaa\"." << endl;
-	}
-	Sheep(string name) : Animal(name) {
-
-	}
+	~Sheep() { }
+	void speak() const { cout << "    Sheep " << getName() << " says \"baaa\"." << endl; }
+	Sheep(string name) : Animal(name) { }
 };
-
 class Dog : public Animal {
 public:
-	~Dog() {
-
-	}
-	void speak() const {
-		cout << "    Dog " << getName() << " says \"woof\"." << endl;
-	}
-	Dog(string name) : Animal(name) {
-
-	}
+	~Dog() { }
+	void speak() const { cout << "    Dog " << getName() << " says \"woof\"." << endl; }
+	Dog(string name) : Animal(name) { }
 };
-
 class Flock {
 public:
-	Flock(string dogName) {
-		dog = new Dog(dogName);
-	}
+	Flock(string dogName) { dog = new Dog(dogName); }
 	virtual ~Flock() {
 		for (int i = 0; i < sheepList.size(); i++) {
 			delete sheepList[i];
 		}
 		delete dog;
 	}
-	void addSheep(string name) {
-		sheepList.push_back(new Sheep(name));
-	}
+	void addSheep(string name) { sheepList.push_back(new Sheep(name)); }
 	void soundOff() {
 		cout << "The flock of " << sheepList.size() << " sheep speaks!" << endl;
 		dog->speak();
@@ -72,18 +46,3 @@ private:
 	Dog *dog;
 	vector<Sheep*> sheepList;
 };
-
-int main() {
-	Flock *myFlock = new Flock("Spot");
-	myFlock->soundOff();
-	myFlock->addSheep("Daisy");
-	myFlock->addSheep("Clover");
-	myFlock->addSheep("Estelle");
-	myFlock->soundOff();
-	delete myFlock;
-	myFlock = new Flock("Rover");
-	myFlock->addSheep("Butch");
-	myFlock->addSheep("Jonno");
-	myFlock->soundOff();
-	delete myFlock;
-}
